f42727a240cfeac747bc18c92ccddd8a194adc61
Ben Brostoff <ben.brostoff@gmail.com>
2014-06-25 23:48:48 -0400

    Added user seeds

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+require 'faker'
+
+User.create(first_name: "Ben", last_name: "Brostoff", email: "ben.brostoff@gmail.com", password_hash: "test")
+User.create(first_name: "Brendan", last_name: "Susens-Jackson", email: "bsusensjackson@gmail.com" , password_hash: "test")
+User.create(first_name: "Armen", last_name: "Vartan", email: "vartan.armen@gmail.com", password_hash: "test")
+User.create(first_name: "Kai", last_name: "Prout", email: "kaiprt@gmail.com", password_hash: "test")
+User.create(first_name: "Natalie", last_name: "Baer", email: "ncbaer@gmail.com", password_hash: "test")
+User.create(first_name: "Katie", last_name: "Reiner", email: "kreiner@umich.edu", password_hash: "test")
+User.create(first_name: "Lexi", last_name: "Ernst", email: "ldernst@umich.edu", password_hash: "test")
+User.create(first_name: "Ken", last_name: "Mendonca", email: "mendoca.kr@gmail.com", password_hash: "test")
+User.create(first_name: "Drew", last_name: "Teter", email: "mteter13@gmail.com" , password_hash: "test")
