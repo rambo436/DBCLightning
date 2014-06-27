fa67b5f1b80d3573d56fe5debd8e17771d5db387
Kai Prout <kaiprt@gmail.com>
2014-06-27 18:54:44 -0400

    Added tags

--- app/controllers/tags.rb
+++ app/controllers/tags.rb
@@ -4,5 +4,6 @@ get '/tags/:tag_id/talks' do # View all talks with specific tag.
 end
 
 get '/tags/:tag_id/users' do # View all users that used a specific tag.
+  @speakers = User.where
   erb :'tags/users_by_tag'
 end
