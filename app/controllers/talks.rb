75aa94dfe559eedc332e86ff9c7ea7800edd458e
Armen Vartan <vartan.armen@gmail.com>
2014-06-27 10:57:51 -0400

    added @talk instance variable to all files in controller

--- app/controllers/talks.rb
+++ app/controllers/talks.rb
@@ -4,26 +4,31 @@ get '/talks' do
 end
 
 get 'talks/:tid' do
+  @talk = Talk.find(params[:tid])
 
   erb :'/talks/talk'
 end
 
 get 'talks/:tid/new' do
+  @talk = Talk.find(params[:tid])
 
   erb :'talks/new'
 end
 
 get 'talks/:tid/edit' do
+  @talk = Talk.find(params[:tid])
 
   erb :'talks/edit'
 end
 
 put 'talks/:tid' do
+  @talk = Talk.find(params[:tid])
 
   redirect "/talks"
 end
 
 delete 'talks/:tid' do
+  @talk = Talk.find(params[:tid])
 
   redirect "/talks"
 end
