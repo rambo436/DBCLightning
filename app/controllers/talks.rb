c86295bcf19c35f2719e719c74ab264b47350507
Armen Vartan <vartan.armen@gmail.com>
2014-06-27 11:22:02 -0400

    copied tag parser from another project. it finds or creates edited tags

--- app/controllers/talks.rb
+++ app/controllers/talks.rb
@@ -30,11 +30,18 @@ put 'talks/:tid' do
                 time:        params[:time],
                 min_rsvp:    params[:min_rsvp] )
 
+  tag_parser = params[:tags].gsub(" ", "")
+  tag_namess = tag_parser.split("#").find_all{ |word| word.length > 0 }
+
+  @talk.tags.clear
+  tag_names.each{ |tag_name| @talk.tags << Tag.find_or_create_by(name: tag_name) }
+
   redirect "/talks"
 end
 
 delete 'talks/:tid' do
   @talk = Talk.find(params[:tid])
+  @talk.destroy
 
   redirect "/talks"
 end
