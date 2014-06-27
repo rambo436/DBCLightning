631846e376985c735ecd6adb5c292b0a3e2758e0
Ben Brostoff <ben.brostoff@gmail.com>
2014-06-27 16:53:30 -0400

    brought talks views and controller up to the times

--- app/controllers/talks.rb
+++ app/controllers/talks.rb
@@ -4,24 +4,25 @@ get '/talks' do # view all talks #We removed the /edit preceding
 end
 
 get '/talks/:talk_id' do #view specific talk #We removed the /edit preceding
-  @talks = Talk.find(params[:talk_id])
+  @talk = Talk.find(params[:talk_id])
   erb :'talks/single_talk'
 end
 
 get '/talks/:talk_id/edit' do # edit one of your talks #We switched the order of edit and :talk_id
   # Now working with event_time - Armen to lead parsing effort :-)
+  @talk = Talk.find(params[:talk_id])
   erb :edit_single_talk
 end
 
-put '/talks/:talk_id' do #We switched the order of edit and :talk_id
-  @talk = Talk.find(params[:talk_id])
-  @talk.update( title:       params[:title],
-                description: params[:description],
-                event_time:        #PARSER,
-                min_rsvp:    params[:min_rsvp] )
-  # will handle with ajax/jquery
-  redirect '' #somewhere
-end
+# put '/talks/:talk_id' do #We switched the order of edit and :talk_id
+#   @talk = Talk.find(params[:talk_id])
+#   @talk.update( title:       params[:title],
+#                 description: params[:description],
+#                 event_time:        #PARSER,
+#                 min_rsvp:    params[:min_rsvp] )
+#   # will handle with ajax/jquery
+#   redirect '' #somewhere
+# end
 
 delete '/talks/:talk_id' do
   @talk = Talk.find(params[:talk_id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_secure_password
+  #has_secure_password
   has_many :events
   has_many :talks, through: :events, source: :user
   # has_many :presented_talks, through: :events, source: :user
