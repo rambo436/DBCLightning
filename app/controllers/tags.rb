49285e35ceb0a098b93681bd0ef2affd09714901
Kai Prout <kaiprt@gmail.com>
2014-06-27 16:55:21 -0400

    Added functionality to view talks by tag with respective href links

--- app/controllers/tags.rb
+++ app/controllers/tags.rb
@@ -1,7 +1,8 @@
 get '/tags/:tag_id/talks' do # View all talks with specific tag.
-  erb :talks_by_tag
+  @tag = Tag.find(params[:tag_id])
+  erb :'tags/talks_by_tag'
 end
 
 get '/tags/:tag_id/users' do # View all users that used a specific tag.
-  erb :users_by_tag
+  erb :'tags/users_by_tag'
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ActiveRecord::Base
+  validates_presence_of :name
+
   has_many :hashtags
   has_many :talks, through: :hashtags, source: :talk
 end
