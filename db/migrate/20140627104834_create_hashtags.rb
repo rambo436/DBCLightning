0f690cc52e7e475f2a5da2e1caade48a241abda8
Kai Prout <kaiprt@gmail.com>
2014-06-27 12:10:50 -0400

    Deleted migrations folder

--- db/migrate/20140627104834_create_hashtags.rb
+++
@@ -1,9 +0,0 @@
-class CreateHashtags < ActiveRecord::Migration
-  def change
-    create_table :hashtags do |t|
-      t.boolean :presenting?
-      t.boolean :attending?
-      t.timestamps
-    end
-  end
-end
