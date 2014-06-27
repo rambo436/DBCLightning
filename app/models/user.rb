5a1f0600a144ed3203cc18c038da98242f3856ed
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-06-27 10:59:49 -0400

    updated validations for user - changed password and email (removed regex)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ActiveRecord::Base
 	has_secure_password
-  has_many :talks
-  has_many :tags, through: :talks
-	validates :email, :format => {:with => %r{.+@.+\..{2,}}, 
-	:message => "Oops, please enter a valid email." }, :uniqueness => true}
+  has_many :talks, through: :userstalks
+	validates :email, :uniqueness => true
 	validates :first_name, presence: true
 	validates :last_name, presence: true
 
