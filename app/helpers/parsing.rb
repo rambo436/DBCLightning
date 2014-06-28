546b548d03d1a5643324a4eb29d2bc81cf87d91e
Kai Prout <kaiprt@gmail.com>
2014-06-28 19:26:15 -0400

    Added datetime parser by using Ruby time.strftime

--- app/helpers/parsing.rb
+++ app/helpers/parsing.rb
@@ -1,11 +1,15 @@
-helpers do 
+helpers do
 
   def parse_tags(tags)
      format = tags.gsub(" ", "")
      tag_names = format.split("#").find_all{ |word| word.length > 0 } #[yolo, hi]
   end
 
-  def datetime_parse
+  def datetime_am_pm(time)
+    if time.strftime("%I:%M%p")[0] == "0"
+      return time.strftime("%I:%M%p")[1..-1]
+    end
+    time.strftime("%I:%M%p")
   end
 
 end
