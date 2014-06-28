775498bc875c5504fa60291bd3f3eaee71a792b8
Ben Brostoff <ben.brostoff@gmail.com>
2014-06-27 20:06:50 -0400

    Uncommented has_secure_password

--- db/migrate/20140627195655_create_users.rb
+++ db/migrate/20140627195655_create_users.rb
@@ -5,8 +5,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :first_name
       t.string :last_name
       t.string :email
-      t.string :password
-      t.string :password_confirmation
+      t.string :password_digest
 
       t.timestamps
     end
