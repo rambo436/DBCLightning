abacc5ffd1c6b5e3d8715007a919ca225bc1e8ba
Kai Prout <kaiprt@gmail.com>
2014-06-28 15:27:35 -0400

    Added functionality to edit only when the talk is created by you also added gravitars to ul

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,7 @@
-<head>
-  <script src="jquery-1.11.1.min.js"></script>
-</head>
-
-$(document).ready(function() {
-  
-});
+// <head>
+//   <script src="jquery-1.11.1.min.js"></script>
+// </head>
+//
+// $(document).ready(function() {
+//
+// });
