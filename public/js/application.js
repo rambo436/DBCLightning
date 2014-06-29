5325e79419ec07b40f8e5a35c05f7eaf07fe01f1
Kai Prout <kaiprt@gmail.com>
2014-06-29 17:03:12 -0400

    Further progress on Attend / Not Attend logic

--- public/js/application.js
+++ public/js/application.js
@@ -10,14 +10,16 @@ $(document).ready(function() {
     ajaxRequest.done(function(response){
       console.log("Success");
       if (response.attending == true) {
-        $(".up_vote").prop('disabled', true); //disable by class
+        $(".up_vote").prop('disabled', true);
+        $(".down_vote").prop('disabled', false); //disable by class
       };
       if (response.attending == false) {
         $(".down_vote").prop('disabled', true);
+        $(".up_vote").prop('disabled', false);
       };
 
       $("#vote_counter").text(response.votes + ' out of ' + response.min_votes + ' votes needed!');
-
+      //$("#vote_counter").text('CURRENT ATTENDING STAT: ' + response.attending);
     });
 
     ajaxRequest.fail(function(response){
