f116a658da3241192a2ea6e52bb7a81429246871
Kai Prout <kaiprt@gmail.com>
2014-06-30 10:13:14 -0400

    changed db path to localhost

--- config/database.rb
+++ config/database.rb
@@ -34,7 +34,7 @@ DB_NAME = db.path[1..-1]
 
 ActiveRecord::Base.establish_connection(
   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-  :host     => db.host,
+  :host     => db.localhost,
   :port     => db.port,
   :username => db.user,
   :password => db.password,
--- config/environment.rb
+++ config/environment.rb
@@ -18,6 +18,8 @@ require 'logger'
 require 'sinatra'
 require "sinatra/reloader" if development?
 
+require 'faker'
+
 require 'erb'
 
 require 'bcrypt'
