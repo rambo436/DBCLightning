5fe07707534d3c515580423d3217510ca2aeb9a5
Drew Teter <mteter13@gmail.com>
2014-06-30 22:23:19 -0400

    added comments

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -12,12 +12,13 @@ get '/about' do
 end
 
 post '/search' do
-  @talks = Talk.where("title like?", "%#{params["search"]}%")
-  @search = params["search"]
+  @results = get_relavent_results(params[:search]).keys # ranks all talks in order of relavence
+  @results = @results[0..4] # returns top 5 results
   erb :search
 end
 
 def count_word_occurance(talk, word)
+  # counts how many times a word occurs in a talk
   count = 0
   count += talk.title.split(" ").map{|word| word.downcase }.grep(word).length
   count += talk.description.split(" ").map{|word| word.downcase }.grep(word).length
@@ -26,35 +27,21 @@ def count_word_occurance(talk, word)
 end
 
 def something(talk, query)
-  query = params[:search].split(" ").uniq.map { |word| word.downcase }
+  query = params[:search].split(" ").uniq.map { |word| word.downcase } # might want to delete common words like "the" or "and" from the query as they may scew results
   query.map! do |word|
-    count_word_occurance(talk, word)
+    count_word_occurance(talk, word) # counts occurance of each word in each talk 
   end
-  [talk, query.reduce(:+)]
+  [talk, query.reduce(:+)]  # pairs the talk with a basic score
 end
 
 def get_relavent_results(query)
   talks = Talks.all
   talks.map { |talk|
-    something(talk, query)
+    something(talk, query) # returns and array of the talk and an integer that gauges how relavent it is
   }
-  talk_hash = Hash[talks]
-  talk_hash.sort_by { |k, v| v }
+  talk_hash = Hash[talks] # turns the multi-dimentional array into a hash
+  talk_hash.sort_by { |k, v| v } 
 end
-
-# For each talk
-  # For each word in query
-    # counts how many times a word appears in...
-      # title
-      # description
-      # user.first_name
-      # user.last_name
-    # end
-    # sum the occurances of each word
-  # end
-  # sum occurances of all words
-# end
-# sort talks
 
 
 
