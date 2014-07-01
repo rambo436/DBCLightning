9dc8b7eef837ee243edeb5d1dbaf8f6495879538
Drew Teter <mteter13@gmail.com>
2014-06-30 22:25:15 -0400

    moved methods into helper folder

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -17,32 +17,6 @@ post '/search' do
   erb :search
 end
 
-def count_word_occurance(talk, word)
-  # counts how many times a word occurs in a talk
-  count = 0
-  count += talk.title.split(" ").map{|word| word.downcase }.grep(word).length
-  count += talk.description.split(" ").map{|word| word.downcase }.grep(word).length
-  count += 1 if talk.speaker.first_name.downcase.include?(word)
-  count += 1 if talk.speaker.last_name.downcase.include?(word)
-end
-
-def something(talk, query)
-  query = params[:search].split(" ").uniq.map { |word| word.downcase } # might want to delete common words like "the" or "and" from the query as they may scew results
-  query.map! do |word|
-    count_word_occurance(talk, word) # counts occurance of each word in each talk 
-  end
-  [talk, query.reduce(:+)]  # pairs the talk with a basic score
-end
-
-def get_relavent_results(query)
-  talks = Talks.all
-  talks.map { |talk|
-    something(talk, query) # returns and array of the talk and an integer that gauges how relavent it is
-  }
-  talk_hash = Hash[talks] # turns the multi-dimentional array into a hash
-  talk_hash.sort_by { |k, v| v } 
-end
-
 
 
 
